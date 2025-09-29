fc6fe7de8f53ad917367ea87868a76bd3d2c51ec
AJ V <avaithyam@yahoo.com>
2025-09-29 14:23:56 -0400

    initial commit

--- src/main/java/com/pluralsight/MethodPractice.java
+++ src/main/java/com/pluralsight/MethodPractice.java
@@ -2,19 +2,20 @@ package com.pluralsight;
 
 public class MethodPractice {
     public static void main(String[] args) {
-        sayHi();
-        dance();
+        sayHi("ajith", 1600);
+        dance("Salsa");
         fish();
     }
 
-    public static void sayHi() {
-        System.out.println("Hi");
+    public static void sayHi(String firstName, int timeOfDay) {
+        System.out.println("Hi " + firstName + ". The time is " + timeOfDay);
     }
 
-    public static void dance() {
-        System.out.println("Woahhohhh");
-        System.out.println("Stop! ");
-        System.out.println("Hammer Time");
+    public static void dance(String typeOfDancing) {
+//        System.out.println("Woahhohhh");
+//        System.out.println("Stop! ");
+//        System.out.println("Hammer Time");
+        System.out.printf("I am %s dancing\n", typeOfDancing);
     }
 
     public static void fish() {
