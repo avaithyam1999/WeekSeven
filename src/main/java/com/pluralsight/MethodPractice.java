e38208130fa1ccd3683d0c6ae26c065e62c23d20
AJ V <avaithyam@yahoo.com>
2025-09-29 13:40:20 -0400

    initial commit

---
+++ src/main/java/com/pluralsight/MethodPractice.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+
+public class MethodPractice {
+    public static void main(String[] args) {
+        sayHi();
+        dance();
+        fish();
+    }
+
+    public static void sayHi() {
+        System.out.println("Hi");
+    }
+
+    public static void dance() {
+        System.out.println("Woahhohhh");
+        System.out.println("Stop! ");
+        System.out.println("Hammer Time");
+    }
+
+    public static void fish() {
+        System.out.println("Get out fishing rod");
+        System.out.println("Hook the bait");
+        System.out.println("Cast the reel");
+        System.out.println("Wait until you feel a bite");
+    }
+
+}
