a76d5e6e6ba303c7c304d49fdea113139ab54e43
AJ V <avaithyam@yahoo.com>
2025-09-30 09:51:01 -0400

    finished all exercises

--- src/main/java/com/pluralsight/Format.java
+++ src/main/java/com/pluralsight/Format.java
@@ -3,8 +3,15 @@ package com.pluralsight;
 public class Format {
     public static void main(String[] args) {
         formatName("Bob", "Smith");
+        String firstName = "Ajith";
+        String lastName = "Vaithyam";
+        formatName(firstName, lastName);
+        System.out.println(add(4,10));
     }
     public static void formatName(String firstName, String lastName) {
         System.out.println(lastName + ", " + firstName);
+    }
+    public static int add(int x, int y) {
+        return x + y;
     }
 }
